288f2ec34429f107e561a36476bee607c0b7eb27
abivrgs <b2029abivargas@pshs.edu.ph>
2025-08-07 10:29:48 +0800

    1st non graded

---
+++ public/script.js
@@ -0,0 +1,7 @@
+let age = 15;
+let maxAge = 100;
+let numPerDay = 4;
+
+let totalRequired = (numPerDay * 365)*(maxAge - age);
+
+document.write('You will need ' + totalRequired + ' cream o packs to last you until the ripe old age of ' + maxAge + '.');
